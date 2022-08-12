69bf50a026d00e5addd57c159301d47f9634d782
kevindharmawan <kevindharmawan2002@gmail.com>
2022-08-12 18:55:00 +0700

    Added TODOs for commands

--- src/cogs/user.py
+++ src/cogs/user.py
@@ -19,10 +19,23 @@ class UserCommands(commands.Cog):
 
         # TODO: Register user to application via backend
 
+    # Unregister user from application
+    # TODO: Implement
+
     # Send user's settings for current server
     @commands.command()
     async def settings(self, ctx, *args):
+        # TODO: Implement
         pass
+
+    # Edit user's settings
+    # TODO: Implement, can be combined with settings command
+
+    # Set current text channel as default text channel
+    @commands.command()
+    async def channel(self, ctx, *args):
+        channel = GuildService.set_text_channel(self.bot, ctx.message.guild.id, ctx.message.channel.id)
+        await channel.send("Successfully set default text channel to " + ctx.message.channel.name)
 
 def setup(bot):
     bot.add_cog(UserCommands(bot))
