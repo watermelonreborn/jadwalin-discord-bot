64825b8d8dc0cb5bdfd41f22451fffe4638bdef3
kevindharmawan <kevindharmawan2002@gmail.com>
2022-08-26 23:52:51 +0700

    change implementation to single thread

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,6 @@ import asyncio
 import discord
 import os
 import uvicorn
-import threading
 
 from cogs import test, user
 from models.reminder import Reminder
@@ -24,25 +23,27 @@ if TOKEN == None:
     PREFIX = os.getenv("COMMAND_PREFIX")
 
 app = FastAPI()
-bot = commands.Bot(command_prefix=PREFIX)
 
-cogs = [test, user]
-for cog in cogs:
-    cog.setup(bot)
+@app.on_event('startup')
+async def on_startup():
+    bot = commands.Bot(command_prefix=PREFIX)
+    cogs = [test, user]
+    for cog in cogs:
+        cog.setup(bot)
+    asyncio.create_task(bot.start(TOKEN))
+
+    @bot.event
+    async def on_ready():
+        GuildService.bot = bot
+        await bot.change_presence(activity=discord.Game(name="with You"))
+        print(f'{bot.user} has connected to Discord!')
 
 @app.post('/reminder')
 async def reminder(reminders: List[Reminder]):
     asyncio.create_task(MessageService.send_reminder(reminders))
     return {'data': 'OK'}
 
-@bot.event
-async def on_ready():
-    GuildService.bot = bot
-    await bot.change_presence(activity=discord.Game(name="with You"))
-    print(f'{bot.user} has connected to Discord!')
-
 # TODO: Add keyboard interrupt for bot run or use .start
 if __name__ == '__main__':
-    threading.Thread(target=lambda: bot.run(TOKEN)).start()
     uvicorn.run(app, port=5000)
 # bot.run(TOKEN)
